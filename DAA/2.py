c33675999f15c843fa15cf56aeaed58c8e5c2157
Rahul Agrawal <77106010+rahul-agrawal-99@users.noreply.github.com>
2022-11-14 21:10:34 +0530

    .

--- DAA/2.py
+++ DAA/2.py
@@ -1,62 +1,41 @@
-# Huffman Coding in python
+import heapq
 
-string = 'BCAADDDCCACACAC'
+class node:
+	def __init__(self, freq, symbol, left=None, right=None):
+		self.freq = freq
+		self.symbol = symbol
+		self.left = left
+		self.right = right
+		self.huff = ''
+	def __lt__(self, nxt):
+		return self.freq < nxt.freq
+		
 
+def printNodes(node, val=''):
+	newVal = val + str(node.huff)
+	if(node.left):
+		printNodes(node.left, newVal)
+	if(node.right):
+		printNodes(node.right, newVal)
 
-# Creating tree nodes
-class NodeTree(object):
+	if(not node.left and not node.right):
+		print(f"{node.symbol} -> {newVal}")
 
-    def __init__(self, left=None, right=None):
-        self.left = left
-        self.right = right
-
-    def children(self):
-        return (self.left, self.right)
-
-    # def nodes(self):
-    #     return (self.left, self.right)
-
-    # def __str__(self):
-    #     return '%s_%s' % (self.left, self.right)
-
-
-# Main function implementing huffman coding
-def huffman_code_tree(node, left=True, binString=''):
-    if type(node) is str:
-        return {node: binString}
-    (l, r) = node.children()
-    d = dict()
-    d.update(huffman_code_tree(l, True, binString + '0'))
-    d.update(huffman_code_tree(r, False, binString + '1'))
-    return d
-
-
-# Calculating frequency
-freq = {}
-for c in string:
-    if c in freq:
-        freq[c] += 1
-    else:
-        freq[c] = 1
-
-freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
-print(freq)
-nodes = freq
+chars = ['a', 'b', 'c', 'd', 'e', 'f']
+freq = [ 5, 9, 12, 13, 16, 45]
+nodes = []
+for x in range(len(chars)):
+	heapq.heappush(nodes, node(freq[x], chars[x]))
 
 while len(nodes) > 1:
-    (key1, c1) = nodes[-1]
-    (key2, c2) = nodes[-2]
-    nodes = nodes[:-2]
-    node = NodeTree(key1, key2)
-    nodes.append((node, c1 + c2))
-    print("appending node", node, c1 + c2)
+	left = heapq.heappop(nodes)
+	right = heapq.heappop(nodes)
+	left.huff = 0
+	right.huff = 1
+	newNode = node(left.freq+right.freq, left.symbol+right.symbol, left, right)
+	heapq.heappush(nodes, newNode)
 
-    nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+printNodes(nodes[0])
 
-print("nodes[0][0]", nodes[0])
-huffmanCode = huffman_code_tree(nodes[0][0])
 
-print(' Char | Huffman code ')
-print('----------------------')
-for (char, frequency) in freq:
-    print(' %-4r |%12s' % (char, huffmanCode[char]))
+
